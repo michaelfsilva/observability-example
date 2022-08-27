741b0ef1fd78d146c7e350fc31240db63ebddf0c
Michael Silva <michael@snocone.io>
2022-08-27 15:38:27 -0300

    Adding initial structure

---
+++ src/test/kotlin/com/example/observability/observabilityExample/ObservabilityExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.observability.observabilityExample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ObservabilityExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
